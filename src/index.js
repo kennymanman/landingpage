3e163c7cbb32d11bc76b44c78da0e9b64940c36f
kennymanman <61712644+kennymanman@users.noreply.github.com>
2022-01-07 19:21:51 -1200

    font issue

--- src/index.js
+++ src/index.js
@@ -11,6 +11,7 @@ import "./fonts/Recoleta-Light.ttf"
 import "./fonts/RecoletaAlt-Bold.ttf"
 import "./fonts/RecoletaAlt-Medium.ttf"
 import "./fonts/RecoletaAlt-Light.ttf"
+import "./fonts/Doyle-Black.ttf"
 
 
 
